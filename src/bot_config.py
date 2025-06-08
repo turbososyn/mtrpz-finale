c6b6bc7ea7b1279be9b648a8921da36e2dc2008c
turbososyn <turbososyn@gmail.com>
2025-06-08 23:19:05 +0300

    final update

--- src/bot_config.py
+++ src/bot_config.py
@@ -1,6 +1,16 @@
+import os
 import pytz
+from dotenv import load_dotenv
 
-TOKEN = "7590260955:AAFNzZGuSO7pCInqsRRdPFJwUGBMw4TAPMY"
+
+load_dotenv()
+
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+if TOKEN is None:
+
+    raise ValueError("Не знайдено TELEGRAM_BOT_TOKEN. Переконайтесь, що у вас є .env файл з цим значенням.")
 
 TIMEZONE_STR = "Europe/Kyiv"
 TIMEZONE = pytz.timezone(TIMEZONE_STR)
